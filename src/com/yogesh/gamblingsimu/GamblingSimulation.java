7da3e3997c9b1311df48775224188bbd5876cba8
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-03-05 11:53:09 +0530

    [ADDED] : Yogesh Set up of Gambling Simulation Project

---
+++ src/com/yogesh/gamblingsimu/GamblingSimulation.java
@@ -0,0 +1,15 @@
+package com.yogesh.gamblingsimu;
+
+import java.util.Random;
+
+public class GamblingSimulation {
+		
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Gambling Simulating Project"); 
+		
+}
+	
+}
+
