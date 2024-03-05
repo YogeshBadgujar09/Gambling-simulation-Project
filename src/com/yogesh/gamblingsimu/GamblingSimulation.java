9a4aa11b38d837f59699a7cc71d56f38b3872705
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-03-05 13:12:58 +0530

    [ADDED] : Yogesh set per day stack AND bet per game

--- src/com/yogesh/gamblingsimu/GamblingSimulation.java
+++ src/com/yogesh/gamblingsimu/GamblingSimulation.java
@@ -4,10 +4,21 @@ import java.util.Random;
 
 public class GamblingSimulation {
 		
+	private static final int EVERY_DAY_STACK = 100 ;
+	private static final int PER_GAME_STACK = 1 ;
+	
+	public static void gameStatus()
+	{
+		System.out.println("Every Day Stack :" + EVERY_DAY_STACK );
+		System.out.println("Stack Per Game :" + PER_GAME_STACK );
+	}
+	
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Welcome to Gambling Simulating Project"); 
+		System.out.println("Welcome to Gambling Simulating Project");
+		
+		gameStatus();
 		
 }
 	
